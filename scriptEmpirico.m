196ae11a1a350af3d1ecbbd2d0e56b79c400c2ab
Juan Carlos Sigler Priego <jcsiglerp@MacBook-Pro-de-Juan.local>
2020-07-21 18:44:43 -0500

    tabla 2a

--- scriptEmpirico.m
+++ scriptEmpirico.m
@@ -5,14 +5,17 @@ clear; close all; clc;
 casos = 50;
 
 % Inicializamos las variables que vamos a ocupar
+n = zeros(casos, 1);
+m = zeros(casos, 1);
+is_bounded = zeros(casos, 1);
 min_mn = zeros(casos, 1);
 iter = zeros(casos, 1);
 J_bounded = [];
 
 for i = 1:casos
     % Generamos un problema de PL aleatorio
-    [n, m, A, b, c] = generaProblemaAleatorio();
-    min_mn(i) = min(n, m);
+    [n(i), m(i), A, b, c] = generaProblemaAleatorio();
+    min_mn(i) = min(n(i), m(i));
     % Corremos el problema con nuestro método
     [~, ~, ban, iter(i)] = mSimplex_leq(A, b, c);
 
@@ -22,7 +25,10 @@ for i = 1:casos
     end
 end
 
+is_bounded(J_bounded) = 1;
 J_notbounded = setdiff(1:casos, J_bounded);
+
+T = table(n, m, iter, is_bounded)
 
 scatter( min_mn(J_bounded), iter(J_bounded), 'b', 'filled')
 hold on
